ebbbf71c00995359b34909635e5a79e2b2c34e1a
Mike Delucia <mikey970@yahoo.com>
2022-03-11 09:12:50 -0500

    Made header names get pulled from API instead of being written in

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let clicked = false
-
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+  .then(response => response.json())
+  .then(data => leftButton.innerHTML = data.name)
 let logo = document.querySelector(".logo1")
 let logo2 = document.querySelector(".logo2")
   let btc = document.querySelector(".bitcoin")
@@ -21,6 +23,9 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
 
 
 })
+fetch("https://api.coingecko.com/api/v3/coins/ethereum")
+  .then(response => response.json())
+  .then(data => rightButton.innerHTML = data.name)
 let rightButton = document.querySelector(".buttonRight")
 rightButton.addEventListener("click", function (e) {
   fetch("https://api.coingecko.com/api/v3/coins/ethereum")
@@ -37,8 +42,12 @@ fetch("https://api.coingecko.com/api/v3/coins/ethereum")
   .then(data => logo2.src = data.image.small)
 
 
-  })
-  let button3 = document.querySelector(".button3")
+})
+
+let button3 = document.querySelector(".button3")
+fetch("https://api.coingecko.com/api/v3/coins/polkadot")
+.then(response => response.json())
+.then(data => button3.innerHTML = data.name)
   button3.addEventListener("click", function (e) {
     fetch("https://api.coingecko.com/api/v3/coins/polkadot")
     .then(response => response.json())
@@ -55,23 +64,28 @@ fetch("https://api.coingecko.com/api/v3/coins/ethereum")
 
   
   })
-  let button4 = document.querySelector(".button4")
+  
+let button4 = document.querySelector(".button4")
+
+  fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(response => response.json())
+    .then(data => button4.innerHTML = data.name)
   button4.addEventListener("click", function (e) {
     fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(response => response.json())
-    .then(data => btc.innerHTML = data.name)
-  fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(response => response.json())
-    .then(data => btcDesc.innerHTML = data.description.en)
+      .then(response => response.json())
+      .then(data => btc.innerHTML = data.name)
     fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-  .then(response => response.json())
-  .then(data => logo.src = data.image.small)
-  fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-  .then(response => response.json())
-  .then(data => logo2.src = data.image.small)
+      .then(response => response.json())
+      .then(data => btcDesc.innerHTML = data.description.en)
+    fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+      .then(response => response.json())
+      .then(data => logo.src = data.image.small)
+    fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+      .then(response => response.json())
+      .then(data => logo2.src = data.image.small)
 
   
-    })
+  })
   
 
    
